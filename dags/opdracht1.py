767b4e4b4979a800173ca5626244ff51d68f0631
melvinvandenbout <mvandenbout@bol.com>
2019-12-09 11:03:39 +0100

    opdracht 1

---
+++ dags/opdracht1.py
@@ -0,0 +1,23 @@
+import airflow
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+
+args = {
+    'owner': 'Airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+with DAG(
+        dag_id='opdracht 1',
+        default_args=args,
+        dagrun_timeout=None,
+) as dag:
+    task1 = DummyOperator(task_id='task1')
+    task2 = DummyOperator(task_id='task2')
+    task3 = DummyOperator(task_id='task3')
+    task4 = DummyOperator(task_id='task4')
+    task5 = DummyOperator(task_id='task5')
+
+task1 >> task2 >> task3 >> task4 >> task5
+
+
